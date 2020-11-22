03450e1a3da8ba57cfe3fd9716003e4b1b3ab97e
IKurganov <IKurganov@sportmaster.ru>
2020-11-23 00:18:11 +0300

    [*] - Выделение отдельных методов, набросок небольшого теста

--- src/test/java/home/atests/BaseTest.java
+++ src/test/java/home/atests/BaseTest.java
@@ -1,7 +1,8 @@
 package home.atests;
 
-import com.codeborne.selenide.CollectionCondition;
-import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
@@ -12,8 +13,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class BaseTest {
 
-    @Test
-    public void firstTest() throws InterruptedException {
+    @BeforeEach
+    public void setUp() {
+        // пока для упрощения запускаю только хром
+        Configuration.browser = "chrome";
+        Configuration.startMaximized = true;
+        Configuration.reportsFolder = "target/reports";
+        // открываем нужную страницу в браузере и вводим логин
+        openPageAndInsertLogin();
+    }
+
+    private void openPageAndInsertLogin() {
         open("https://mail.ru/");
         $(By.name("login"))
                 .shouldBe(Condition.visible)
@@ -22,27 +32,65 @@ public class BaseTest {
                 .shouldBe(Condition.enabled)
                 .shouldHave(Condition.text("Ввести пароль"))
                 .click();
+    }
 
+    @Test
+    public void loginAndSendLetterTest() throws InterruptedException {
+        // тестируем ввод некорректного пароля, а затем вход и открытие формы для написания письма
+        // находим поле пароля и вводим значение
+        setIncorrectPassword();
+        // ловим ошибку и ее текст
+        //TODO здесь просто надо сразу из PObj возвращать текст и сверять его тут через ассертзет
+        $(By.id("mailbox:error"))
+                .shouldBe(Condition.visible.because("Почта должна показать, что пользователь ввел не те данные"))
+                .has(exactText("Неверное имя или пароль"));
+        // вводим корректный пароль и входим
+        setCorrectPassword();
+        sendLetter();
+    }
+
+    private void sendLetter() {
+        $("a.compose-button_white")
+                .shouldBe(Condition.visible.because("Кнопка Написать письмо должна отображаться"))
+                .click();
+        SelenideElement mainForm = $("div.compose-windows div.compose-app_popup").shouldBe(visible.because("Нажали кнопку для открытия формы"));
+        SelenideElement upperForm = mainForm.$("div.compose-app__compose").shouldBe(Condition.visible.because("Будет место для печати письма"));
+        SelenideElement lowerForm = mainForm.$("div.compose-app__footer")
+                .shouldBe(Condition.visible.because("Также кроме формы будет отображаться панель с кнопками/действиями для письма"));
+        upperForm.$x(".//div[contains(@class, 'editor_container')]//div[contains(@class, 'cke_editable_inline')]/div")
+                .shouldBe(visible.because("Строчка для печати будет видима"))
+                .sendKeys("Хелоу hello Worlddz");
+        //TODO нужно указать адресата
+        lowerForm.$("div.compose-app__buttons span[title='Отправить']").click();
+    }
+
+    private void setCorrectPassword() {
         $(By.name("password"))
                 .shouldBe(Condition.visible)
-                .setValue("F1234567654321");
-
+                .clear();
+        $(By.name("password"))
+                .shouldBe(Condition.visible)
+                .setValue("A1234567654321");
         $(By.id("mailbox:submit-button"))
                 .shouldBe(Condition.enabled)
                 .shouldHave(Condition.text("Войти"))
                 .click();
-        assertThat($(By.id("mailbox:error"))
-                .waitUntil(visible, 1000)
-                .has(Condition.visible)).isTrue();
+        // сообщение об ошибке не будет отображаться
+        $(By.id("mailbox:error")).shouldNotBe(visible);
+    }
 
-        assertThat($(By.id("mailbox:error"))
-                .waitUntil(visible, 1000)
-                .has(exactText("Неверное имя или пароль")))
-                .as("Почта должна уведомить об ошибке и содержать корректный текст")
-                .isTrue();
+    private void setIncorrectPassword() {
+        $(By.name("password"))
+                .shouldBe(Condition.visible)
+                .setValue("F1234567654321");
+        $(By.id("mailbox:submit-button"))
+                .shouldBe(Condition.enabled)
+                .shouldHave(Condition.text("Войти"))
+                .click();
+    }
 
-        /*$$(".results--main .result").shouldHave(CollectionCondition.sizeGreaterThanOrEqual(5));
-        $(".results--main .result").shouldHave(text("heisenbug-moscow.ru"));
-         */
+    @AfterEach
+    public void finishTesting() {
+        WebDriverRunner.getWebDriver().quit();
     }
 }
